3f15f49831bc4ea616601ef22ef73e932b9a9754
jleba <jleba@DESKTOP-MHEG86M>
2022-01-05 11:29:09 -0700

    Add project files.

---
+++ MyFirstASPNETWebApp/wwwroot/js/FanMail.js
@@ -0,0 +1,8 @@
+﻿document.getElementById("btnSend").addEventListener("click", function () {
+    alert("From: " + document.getElementById("txtFrom").value + ", Subject: "
+        + document.getElementById("txtSubject").value + ", Message: "
+        + document.getElementById("txtMessage").value);
+
+
+    $("#picGanderson").fadeToggle("slow");
+})
